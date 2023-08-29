f5a253522328fdc2422951737ccfa5be48f29a0f
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-08-29 16:25:23 -0600

    Inicio de opcion 2

--- codigo.py
+++ codigo.py
@@ -1,18 +1,37 @@
+import datetime
+
 class nota:
-    def __init__():
-        pass
+    def __init__(self, folio, fecha, cliente, monto_a_pagar):
+        self.folio = folio
+        self.fecha = fecha
+        self.cliente = cliente
+        self.monto_a_pagar = monto_a_pagar
     
-    def registrar_nota():
-        pass
+def registrar_nota():
+     pass
 
-    def consultas_reportes():
-       pass
+def consulta_por_periodo():
+    fecha_inicial = input("Ingresa la fecha inicial (dd/mm(yyyy): ")
+    fecha_inicial = datetime.datetime.strptime(fecha_inicial, "%d/%m/%Y").date()
 
-    def cancelar_nota():
-        pass
+    fecha_final = input("Ingresa la fecha final (dd/mm/yyyy): ")
+    fecha_final = datetime.datetime.strptime(fecha_final, "%d/%m/%Y").date()
 
-    def recuperar_nota():
-        pass
+    notas = []
+    for nota in notas:
+        if fecha_final <= nota.fecha <= fecha_final:
+            notas.append(nota)
+    
+    # Imprimir notas en un reporte tabular
+
+def consulta_por_folio():
+    pass
+
+def cancelar_nota():
+    pass
+
+def recuperar_nota():
+    pass
 
 
 print("*** TALLER MECANICO ***")
@@ -30,7 +49,14 @@ while True:
     if opcion == 1:
         pass
     elif opcion == 2:
-        pass
+        print("Submenu Consultas y reportes")
+        opcion = input("Elige una opcion \n1.Consulta por periodo\n2.Consulta por folio")
+        if opcion == 1:
+            consulta_por_periodo()
+        elif opcion == 2:
+            pass
+        else:
+            print("Opcion no valida")
     elif opcion == 3:
         pass
     elif opcion == 4:
