af5ce56d9e47bdc2091babe97236d467ce0e2023
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-08-28 18:00:19 -0600

    Inicializar programa

---
+++ codigo.py
@@ -0,0 +1,25 @@
+print("*** TALLER MECANICO ***")
+print("*** BIENVENIDO A NUESTRO SISTEMA DE NOTAS ***")
+
+while True:
+    print("MENU")
+    print("1. Registrar nota")
+    print("2. Consultas y Reportes")
+    print("3. Cancelar nota")
+    print("4. Recuperar nota")
+    print("5. Salir del sistema")
+
+    opcion = int(input("Elige una opcion: "))
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        break
+
+
+    
