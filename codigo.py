b1c3b1b6396decc6b242e062131651a060ef8988
Norma Lizeth <143469979+LizethRmzz@users.noreply.github.com>
2023-08-31 19:18:50 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -8,7 +8,53 @@ class Nota:
         self.monto_a_pagar = monto_a_pagar
     
 def registrar_nota():
-     pass
+    import uuid
+    import datetime
+
+def main():
+    cliente = input("Nombre del cliente: ")
+    nota = Nota(cliente)
+    while True:
+        nombre_servicio = input("Nombre del servicio requerido (f para finalizar)): ")
+        if nombre_servicio.lower() == "f":
+            break
+        costo_servicio = float(input("Costo del servicio: "))
+        while costo_servicio <= 0:
+            print("El costo debe ser mayor que 0.")
+            costo_servicio = float(input("Ingrese el costo del servicio: "))
+        servicio = Servicio(nombre_servicio, costo_servicio)
+        nota.agregar_servicio(servicio)
+    monto_total = nota.calcular_monto_total()
+    print("\n---------------NOTA-------------")
+    print(f"Folio: {nota.folio}")
+    print(f"Fecha: {nota.fecha}")
+    print(f"Cliente: {nota.cliente}")
+    print("--------------------------------")
+    print("Servicio:")
+    for servicio in nota.servicios:
+        print(f"- {servicio.nombre}: ${servicio.costo:.2f}")
+    print("--------------------------------")
+    print(f"Total a pagar: ${monto_total:.2f}")
+
+class Servicio:
+    def __init__(self, nombre, costo):
+        self.nombre = nombre
+        self.costo = costo
+
+class Nota:
+    def __init__(self, cliente):
+        self.folio = str(uuid.uuid4())[:5]
+        self.fecha = datetime.date.today() 
+        self.cliente = cliente
+        self.servicios = []
+    def agregar_servicio(self, servicio):
+        self.servicios.append(servicio)
+    def calcular_monto_total(self):
+        total = sum(servicio.costo for servicio in self.servicios)
+        return total
+
+if __name__ == "__main__":
+    main()
 
 def consulta_por_periodo():
     fecha_inicial = input("Ingresa la fecha inicial (dd/mm(yyyy): ")
